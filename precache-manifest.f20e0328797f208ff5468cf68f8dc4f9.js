15b85b5bf90befedf76bf5239572f553d27a20b9
imrizwan <razzyshaikh@gmail.com>
2019-08-10 20:04:31 +0500

    2nd

---
+++ precache-manifest.f20e0328797f208ff5468cf68f8dc4f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa481b1d467ea82f90b417809e910c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1413fbb18a77c90bba6",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f45a0ffd5f7c37fad01a",
+    "url": "/static/css/main.31006fe3.chunk.css"
+  },
+  {
+    "revision": "f1413fbb18a77c90bba6",
+    "url": "/static/js/2.f442f6cf.chunk.js"
+  },
+  {
+    "revision": "f45a0ffd5f7c37fad01a",
+    "url": "/static/js/main.8ca11a94.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f1cdf7dddfd83441c6e2add40dbd704c",
+    "url": "/static/media/background.f1cdf7dd.jpg"
+  }
+]);
