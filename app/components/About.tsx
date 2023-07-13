1590cacab11959df83f55fcdd7ff453417fc18e9
Master <razzyshaikh@gmail.com>
2023-07-13 22:23:24 +0400

    eslint fix

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -4,10 +4,10 @@ export default function About() {
       <h2 className="text-4xl font-bold text-blue-600">About Me</h2>
       <p className="mt-8 text-lg text-gray-700 leading-relaxed">
         <span className="font-semibold">
-          Hello! I'm a Full Stack Developer.
+          Hello! I&apos;m a Full Stack Developer.
         </span>{" "}
         With 4 years of professional experience in front-end and back-end
-        development, I've honed my skills in React and Node.js. I leverage
+        development, I&apos;ve honed my skills in React and Node.js. I leverage
         open-source platforms, tools, front-end frameworks, and middleware to
         modernize enterprise applications by redeveloping legacy systems.
       </p>
