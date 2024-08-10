6a3fd6c73ac192c43dad76e55432794b66f0f1f4
Rizwan <razzyshaikh@gmail.com>
2024-08-11 02:17:14 +0500

    headers removed

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -2,22 +2,7 @@
 
 import nodemailer from 'nodemailer';
 
-export async function POST(req, res) {
-
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
-  // specific logic for the preflight request
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  
+export async function POST(req, res) {  
   const { name, email, subject, message } = await req.json();
 
   if (!name || !email || !subject || !message) {
